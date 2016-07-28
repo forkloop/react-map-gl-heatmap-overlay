c3b36b3771df4f9b282733fb272bc569f032a9d2
Xin Liu <xliu@classpass.com>
2016-07-27 22:23:39 -0400

    wip

--- example/main.js
+++ example/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var polyfill = require("babel-polyfill");
 var document = require('global/document');
 var window = require('global/window');
 var React = require('react');
@@ -11,37 +12,50 @@ var HeatmapOverlay = require('../');
 var assign = require('object-assign');
 var locations = require('example-cities');
 var rasterTileStyle = require('raster-tile-style');
+var socketio = require('socket.io-client');
+var $ = require("jquery");
 
-var tileSource = '//tile.stamen.com/toner/{z}/{x}/{y}.png';
-var mapStyle = Immutable.fromJS(rasterTileStyle([tileSource]));
 
 function color() {
-  return 'rgb(' +
-    Math.floor(Math.random() * 256) + ', ' +
-    Math.floor(Math.random() * 256) + ', ' +
-    Math.floor(Math.random() * 256) +
-  ')';
+  return 'rgb(35, 207, 185)';
 }
+
+// {{'latitude', 'longitude'}, expire}
+var cache = [];
 
 var App = React.createClass({
 
   displayName: 'App',
+
+  loadInitialData: function loadInitialData() {
+      var app = this;
+      for (var i = 0; i < locations.length; i++) {
+          cache.push({point: locations[i], timeout: Math.floor(Math.random() * 200 + 60)});
+      }
+      app.setState({points: locations});
+      //$.ajax({
+      //    url: '',
+      //}).done(function() {
+      //    app.setState({points: locations});
+      //});
+  },
 
   getInitialState: function getInitialState() {
     return {
       viewport: {
         width: window.innerWidth,
         height: window.innerHeight,
-        latitude: 0,
-        longitude: 0,
-        zoom: 2
+        latitude: 39.50,
+        longitude: -98.25,
+        zoom: 3
       },
+      points: [],
       gradientColors: Immutable.List([color(), color()]),
-      mapStyle: mapStyle
     };
   },
 
   componentDidMount: function componentDidMount() {
+    this.loadInitialData();
     window.addEventListener('resize', function onResize() {
       this.setState({
         viewport: assign({}, this.state.viewport, {
@@ -53,6 +67,7 @@ var App = React.createClass({
     window.setInterval(function setInterval() {
       this.setState({gradientColors: Immutable.List([color(), color()])});
     }.bind(this), 2000);
+    this.pulse();
   },
 
   _onChangeViewport: function _onChangeViewport(viewport) {
@@ -62,10 +77,10 @@ var App = React.createClass({
   render: function render() {
     return r(MapGL, assign({}, this.state.viewport, {
       onChangeViewport: this._onChangeViewport,
-      mapStyle: this.state.mapStyle
+      mapboxApiAccessToken: 'pk.eyJ1IjoiZm9ya2xvb3AiLCJhIjoiY2lyNWphMmt6MDA3amdjbmZkd2drazNqOSJ9.65mr0gv1_YWV2bd-NOlRHQ',
     }), [
       r(HeatmapOverlay, assign({}, this.state.viewport, {
-        locations: locations,
+        locations: this.state.points,
         // Semantic zoom
         sizeAccessor: function sizeAccessor() {
           return 60;
@@ -77,10 +92,35 @@ var App = React.createClass({
         // }
       }))
     ]);
-  }
+  },
+
+    pulse: function pulse() {
+      window.setInterval(function(app) {
+          return function() {
+              var index = Math.floor(Math.random() * locations.length);
+              cache.push({point: locations[index], timeout: Math.floor(Math.random() * 300)});
+              var points = [];
+              var c = [];
+              for (var i = 0; i < cache.length; i++) {
+                  var timeout = cache[i].timeout;
+                  if (--timeout > 0) {
+                      points.push(cache[i].point);
+                      c.push({point: cache[i].point, timeout: timeout});
+                  }
+              }
+              cache = c;
+              app.setState({points: points});
+          }
+      }(this), 1000);
+    },
+
 });
+
+var io = socketio();
+console.log(io);
 
 var reactContainer = document.createElement('div');
 document.body.style.margin = '0';
 document.body.appendChild(reactContainer);
 ReactDOM.render(r(App), reactContainer);
+
